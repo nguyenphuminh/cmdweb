35002f6aa9539029bb65c70456a2965ed9cef143
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2024-06-14 20:34:23 +0700

    Update examples

--- examples/handle_add.bat
+++ examples/handle_add.bat
@@ -4,6 +4,5 @@ call lib/get_request %~1
 
 set res.body=%req.params.name%
 set res.statusCode=200
-set res.headers={"content-type":"text/html"}
 
 call lib/respond %~1
--- examples/handle_main.bat
+++ examples/handle_main.bat
@@ -4,5 +4,6 @@ call lib/get_request %~1
 
 set res.body=Beep Boop!
 set res.statusCode=200
+set res.headers={"content-type":"text/html"}
 
 call lib/respond %~1

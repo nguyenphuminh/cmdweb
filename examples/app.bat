00108be1a5807ebfef8c55491f1c14f913ac4f4a
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2024-06-30 12:05:42 +0700

    Add lib/start
    It helps make it more intuitive and if we migrate the engine to a different language/format in the future, the user does not need to change their code

--- examples/app.bat
+++ examples/app.bat
@@ -9,4 +9,4 @@ call lib/listen "get" "/hello" handle_hello
 call lib/listen "post" "/add/:name" handle_add
 
 :: Start the js runtime
-node lib/engine 5000 "127.0.0.1"
+call lib/start 5000 "127.0.0.1"
---
+++ lib/start.bat
@@ -0,0 +1,1 @@
+node lib/engine %~1 %~2 %~3 %~4
